7165e20a57f1d524e873fe88350ba5d2ead419a2
Sagnik Chattopadhyaya <36898274+sagnik20@users.noreply.github.com>
2022-02-10 15:39:59 +0530

    Create Question2.java

---
+++ week2/Question2.java
@@ -0,0 +1,64 @@
+import java.util.*;
+import java.io.*;
+import java.lang.Math;
+
+public class Main
+{
+  public static int setPassword(int N,int[] A)
+  {
+
+    //this is default OUTPUT. You can change it.
+    int result = -404;
+    String  x="",y="";
+    Stack a = new Stack();
+    Stack b = new Stack();
+    for(Integer i=0;i<A.length;;i++){            
+            Integer k = A[i];
+            if(k%2==0 && k%3!=0){
+                    a.push(k);
+            }
+            else if(k%2==0 && k%3 ==0){
+                    if(!a.empty())
+                    a.pop();
+            }
+            else if(k%2!=0 && k%3!=0){
+                    b.push(k);
+            }
+            else if(k%2!=0 && k%3==0){
+                    if(!b.empty())
+                    b.pop();
+            }
+    }
+        if(!a.empty())
+        x=""+a.pop();
+        else 
+        x="00";
+
+        if(!b.empty())
+        y=""+b.pop();
+        else
+        y="00";
+        System.out.println(x+""+y);
+
+
+      return result;
+  }
+  public static void main (String[]args)
+  {
+    Scanner sc = new Scanner (System.in);
+
+    //INPUT [uncomment & modify if required]
+    int N = sc.nextInt ();
+
+    int[] A = new int[N];
+    for (int i = 0; i < N; i++)
+    {
+        A[i] = sc.nextInt();
+    }
+    
+    sc.close ();
+
+    //OUTPUT [uncomment & modify if required]
+    setPassword(N,A);
+  }
+}
