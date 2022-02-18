2bdca83ddc16fa232ca419f90c509b48ebdb4900
Sagnik Chattopadhyaya <36898274+sagnik20@users.noreply.github.com>
2022-02-18 19:28:03 +0530

    Update Question1.java

--- week2/Question1.java
+++ week2/Question1.java
@@ -1,55 +1,27 @@
 import java.util.*; 
-
 import java.lang.*;
-
 import java.io.*;
-
 import java.lang.Math;
 
-
-
 public class Main {
-
         public static String popStack(String S) {
-
-                //this is default OUTPUT. You can change it.
-
                 String X="";
-
                 Integer l = S.length();
 
                 for(Integer i=0;i<l;i++){
-                        char c = S.charAt(i);
+                       int a = (int) S.charAt(i);
 
-                        int a = (int) c;
-
-                        if(a%3==0)
-                                X = c+X;
-
+                       if(a%3==0)
+                                X = (int) a + X;
                 }
-
-               
-
                 return X;
-
         }
 
         public static void main(String[] args) {
-
-                // INPUT [uncomment & modify if required]
-
                 Scanner sc = new Scanner(System.in);
-
                 String S = sc.next();
 
-                
-
-                // OUTPUT [uncomment & modify if required]
-
                 System.out.print(popStack(S));
-
                 sc.close();
-
         }
-
 }
